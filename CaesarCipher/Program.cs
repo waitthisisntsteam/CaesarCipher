dc1fa3a1df13c60e40bdbaf099470ce591111504
erik <lapizcraft4@gmail.com>
2020-07-27 12:22:21 -0700

    Finished Caesar Cypher but couldn't import dll.

--- CaesarCipher/Program.cs
+++ CaesarCipher/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using linkedLists;
 
 namespace CaesarCipher
@@ -7,32 +8,44 @@ namespace CaesarCipher
     {
         static public string CaesarCypherEncrypt(int key, string phrase)
         {
-            string[] EncyptedPhrase = new string[phrase.Length];
+            char Encryptor = new DoublyLinkedList<char>();
+            string EncryptedPhrase = "";
+            char finalLetter = ' ';
             for (int i = 0; i < phrase.Length; i++)
             {
                 for (char currentLetter = 'a'; currentLetter <= 'z'; currentLetter++)
                 {
-                    if (phrase[i] == currentLetter)
+                    if (phrase[i] == ' ')
                     {
-                        //for (char finalLetter = currentLetter; finalLetter <= )
-                        //EncyptedPhrase = currentLetter + key;
+                        Encryptor.AddNodeToLast(' ');
+                        break;
+                    }
+                    else if (phrase[i] == currentLetter)
+                    {
+                        finalLetter = currentLetter;
+                        for (int j = 0; j < key; j++)
+                        {
+                            finalLetter++;
+                        }
+                        Encryptor.AddNodeToLast(finalLetter);
                     }
                 }
             }
+            var runner = Encryptor.Head;
+            for (int i = 0; i < Encryptor.Count; i++)
+            {
+                EncryptedPhrase += runner;
+                runner = runner.Next;
+            }
+            return EncryptedPhrase;
         }
         static void Main(string[] args)
         {
-            var list = new DoublyLinkedList<char>();
-
-            for (char currentLetter = 'a'; currentLetter <= 'z'; currentLetter++)
-            {
-                list.AddNodeToLast(currentLetter);
-            }
-
             Console.WriteLine("Enter a key for the Caesar Cypher.");
             int key = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter a phrase for the Caesar Cypher.");
             string phrase = Console.ReadLine();
+            Console.WriteLine(CaesarCypherEncrypt(key, phrase));
         }
     }
 }
